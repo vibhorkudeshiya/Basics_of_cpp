096352046c6edc5e05ff5e3f1e1c3ce9ac73ef16
vibhorkudeshiya <142561081+vibhorkudeshiya@users.noreply.github.com>
2023-10-16 17:08:47 +0530

    Create number_in_word.cpp

---
+++ number_in_word.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int dig;
+    cout<<"Enter a digit- ";
+    cin>>dig;
+    switch(dig){
+    case 0: cout<<"Zero"; break;
+    case 1: cout<<"one"; break;
+    case 2: cout<<"Two"; break;
+    case 3: cout<<"Three"; break;
+    case 4: cout<<"Four"; break;
+    case 5: cout<<"five"; break;
+    case 6: cout<<"Six"; break;
+    case 7: cout<<"Seven"; break;
+    case 8: cout<<"eight"; break;
+    case 9: cout<<"nine"; break;
+    case 10: cout<<"ten"; break;
+    case 11: cout<<"eleven"; break;}
+}
