7b14cef1b038bd1782269e8624bbc7b4bfcf6abb
vibhorkudeshiya <142561081+vibhorkudeshiya@users.noreply.github.com>
2023-10-16 17:03:20 +0530

    Create calculator.cpp

---
+++ calculator.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int Var1,Var2,Var3;
+    cout<<"Enter number 1-"<<endl;
+    cin>>Var1;
+    cout<<"Enter number 2-"<<endl;
+    cin>>Var2;
+    cout<<"Sum of given inputs="<<Var1+Var2<<endl;
+    cout<<"Difference of given inputs="<<Var1-Var2<<endl;
+    cout<<"Product of given inputs="<<Var1*Var2<<endl;
+    cout<<"Division of given inputs="<<Var1/Var2<<endl;
+    cout<<"Enter number 3-"<<endl;
+    cin>>Var3;
+    cout<<"Asignment="<<Var3+=Var2<<endl;
+}
